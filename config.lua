c49122834dea30e47511470d086c3e5206027716
RuubTv <rubeneleveld@msn.com>
2026-03-28 21:10:31 +0200

    Improve comments in config.lua for better understanding
    Updated comments for clarity and consistency in the config file.

--- config.lua
+++ config.lua
@@ -1,18 +1,17 @@
 Config = {}
 
-Config.Interact = "ox_target" -- "ox_target" of "sleepless" als je die gebruikt
+Config.Interact = "ox_target" -- The resource used for the target/interact system.
 
--- Elke entry hier is één evidence-locatie
--- Tip: maak per job een aparte entry, ook als coords hetzelfde zijn
+-- Every Entry is a locker, you can have as many lockers as you want, just copy paste the template and change the coords and job
 Config.EvidenceLockers = {
-  ["MRPD_Police"] = {
-    coords      = vector3(443.53, -974.54, 30.68),
-    jobs        = { "police" },   -- alleen politie, kan gedeeld worden met meerdere jobs als rangen overeenkomen
-    withdrawRank= 2,              -- min. grade om UIT locker te halen
-    clearRank   = 3,              -- min. grade om locker te legen
-    deleteRank  = 4,              -- min. grade om locker te verwijderen
-    stashWeight = 500000,
-    stashSlots  = 50,
+  ["MRPD_Police"] = {               -- name of the locker, has to be unique, this is used for the target and the database, so choose wisely
+    coords      = vector3(443.53, -974.54, 30.68),      -- coords of the locker, this is where the target will be and where you can interact with the locker
+    jobs        = { "police" },   -- jobs that can access this locker min. 1 required
+    withdrawRank= 2,              -- min. grade to widhdraw items from locker (grade 0 is the lowest, grade 1 is the next etc.)
+    clearRank   = 3,              -- min. grade to clear locker
+    deleteRank  = 4,              -- min. grade to delete locker
+    stashWeight = 500000,         -- weight of the locker stash
+    stashSlots  = 50,             -- slots of the locker stash  
   },
 
   ["Sandy_Police"] = {
@@ -36,3 +35,4 @@ Config.EvidenceLockers = {
     stashSlots  = 40,
   }
 }
+
